9225528b8d7c420c3d41c12b086ecb5cf882bd28
Jason Shifflet <jason@pikimal.com>
2016-12-11 19:38:27 -0500

    New code.

---
+++ draw.js
@@ -0,0 +1,92 @@
+var cy;
+function setGraphs(graph_names) {
+  var graphs_to_render = [];
+  for(i=0; i < graph_names.length; i++){
+    graphs_to_render = graphs_to_render.concat(graphs[graph_names[i]]);
+    console.log(graphs[graph_names[i]]);
+  }
+  return drawGraph(graphs_to_render);
+}
+
+function useCircleLayout(){
+	return  { name: 'circle', 
+  fit: true, // whether to fit the viewport to the graph
+  padding: 30, // the padding on fit
+  boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+  avoidOverlap: true, // prevents node overlap, may overflow boundingBox and radius if not enough space
+  radius: undefined, // the radius of the circle
+  startAngle: 3 / 2 * Math.PI, // where nodes start in radians
+  sweep: undefined, // how many radians should be between the first and last node (defaults to full circle)
+  clockwise: true, // whether the layout should go clockwise (true) or counterclockwise/anticlockwise (false)
+  sort: undefined, // a sorting function to order the nodes; e.g. function(a, b){ return a.data('weight') - b.data('weight') }
+  animate: false, // whether to transition the node positions
+  animationDuration: 500, // duration of animation in ms if enabled
+  animationEasing: undefined, // easing of animation if enabled
+  ready: undefined, // callback on layoutready
+  stop: undefined // callback on layoutstop
+};
+}
+
+function drawGraph(graphs_to_render) {
+  var graphs_to_render_compacted = [];
+  var elements = graphs_to_render;
+ 
+  console.log(elements);
+  
+  cy = cytoscape({
+    // very commonly used options
+  container: document.getElementById("cy"),
+  elements: elements,
+  style: [ // the stylesheet for the graph
+    {
+      selector: 'node',
+      style: {
+        'background-color': '#666',
+        'label': 'data(id)'
+      }
+    },
+
+    {
+      selector: 'edge',
+      style: {
+        'width': 3,
+        'line-color': '#ccc',
+        'target-arrow-color': '#ccc',
+        'target-arrow-shape': 'triangle'
+      }
+    }
+  ],
+  layout: useCircleLayout(),
+
+  // initial viewport state:
+  zoom: 1,
+  pan: { x: 0, y: 0 },
+
+  // interaction options:
+  minZoom: 1e-50,
+  maxZoom: 1e50,
+  zoomingEnabled: true,
+  userZoomingEnabled: true,
+  panningEnabled: true,
+  userPanningEnabled: true,
+  boxSelectionEnabled: false,
+  selectionType: 'single',
+  touchTapThreshold: 8,
+  desktopTapThreshold: 4,
+  autolock: false,
+  autoungrabify: false,
+  autounselectify: false,
+
+  // rendering options:
+  headless: false,
+  styleEnabled: true,
+  hideEdgesOnViewport: false,
+  hideLabelsOnViewport: false,
+  textureOnViewport: false,
+  motionBlur: false,
+  motionBlurOpacity: 0.2,
+  wheelSensitivity: 1,
+  pixelRatio: 'auto'});
+
+  return cy;
+}
--- runner.py
+++ runner.py
@@ -46,7 +46,7 @@ for file in files:
     result_array = [file, []]
     results_arrays.append(result_array)
 
-    fd = open(file, "r")
+    fd = open(file, "r", errors="ignore")
     
     st = parser.suite(fd.read())
 
@@ -58,87 +58,125 @@ for file in files:
     fd.close()
 
 
-
+graphs = {}
 nodes_by_name = {}
+nodes_by_file = {}
 nodes_by_id = []
 num_nodes = 0 
 
 for results_for_file in results_arrays:
     file_name = results_for_file[0]
-     
+    edges = []
+    
+   
+
     for results_for_name in results_for_file[1]:
         if results_for_name[0] not in nodes_by_name:
-            nodes_by_name[results_for_name[0]] = []
+            new_node = {"data": {"id": results_for_name[0]}}
+            nodes_by_name[results_for_name[0]] = [new_node]
+        
+        file_node_line = {"data": {"id": file_name + ": " + str(results_for_name[1])}}
 
+        if file_name not in nodes_by_file:
+            nodes_by_file[file_name] = [{"data": {"id": file_name }}]
+            nodes_by_file[file_name].append(new_node)
+        else:
+            nodes_by_file[file_name].append(new_node)
+ 
+                    
+        nodes_by_name[results_for_name[0]].append(file_node_line)
 
-        nodes_by_id.append({"id": num_nodes + 1, "label": file_name + ": " + results_for_name[0] + " -> " + str(results_for_name[1])})
-        nodes_by_name[results_for_name[0]].append(nodes_by_id[num_nodes])
+            
 
         num_nodes += 1
 
-print(json.dumps(nodes_by_name))
-              
-edges = []
+
+for file_name in nodes_by_file:
+    nodes_to_work_on = nodes_by_file[file_name]
+
+    edges = []
+    nodes = []
+    index = 0
+
+    for node in nodes_to_work_on:
+        if index == 0:
+            source_node = node
+        else:
+            node_to_link_to = node
+            
+            edges.append({"data": {"id": str(source_node["data"]["id"]) + '-' + str(node_to_link_to["data"]["id"]),"source": str(source_node["data"]["id"]), "target": str(node_to_link_to["data"]["id"])}})
+
+
+        num_nodes += 1
+        index += 1
+        nodes.append(node)
+    graphs[file_name] = nodes + edges
 
 for node_name in nodes_by_name:
-    nodes = nodes_by_name[node_name]
+    nodes_to_work_on = nodes_by_name[node_name]
  
+    num_linked_nodes = 0
+    nodes = []
+    edges = []
     index = 0
-    for node in nodes:
+    for node in nodes_to_work_on:
         if index == 0:
-            nodes_by_id.append({"id": num_nodes + 1, "label": node_name})
+            source_node = node    
+        else:
+            node_to_link_to = node
+            edges.append({"data": {"id": str(source_node["data"]["id"]) + '-' + str(node_to_link_to["data"]["id"]),"source": str(source_node["data"]["id"]), "target": str(node_to_link_to["data"]["id"])}})
             num_nodes += 1
-   
-        edges.append({"from": num_nodes, "to": node["id"]})
+            
+        nodes.append(node)   
         index += 1
+    graphs[node_name] = nodes + edges
     
-print(json.dumps(edges))
 
 
 html = """<!doctype html>
 <html>
 <head>
+  <style>
+    #cy {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        display: block;
+    }
+</style>
+
+<script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
+		<script src="cytoscape.js"></script>
+
+<script src="data.js"></script>
+
+<script src="draw.js"></script>
+</head>
   <title>Network | Basic usage</title>
 
-  <script type="text/javascript" src="vis.js"></script>
-  <link href="vis.css" rel="stylesheet" type="text/css" />
 
-  <style type="text/css">
-    #mynetwork {
-      width: 600px;
-      height: 400px;
-      border: 1px solid lightgray;
-    }
-  </style>
-</head>
-<body>
+<body width="100% height="100%">
 
 <p>
   Create a simple network with some nodes and edges.
 </p>
 
-<div id="mynetwork"></div>
+<div id="cy" width="100%" height="100%"></div>
 
-<script type="text/javascript">
-  // create an array with nodes
-  var nodes = new vis.DataSet(""" + json.dumps(nodes_by_id) + """);
 
-  // create an array with edges
-  var edges = new vis.DataSet(""" + json.dumps(edges) + """);
-
-  // create a network
-  var container = document.getElementById('mynetwork');
-  var data = {
-    nodes: nodes,
-    edges: edges
-  };
-  var options = {};
-  var network = new vis.Network(container, data, options);
-</script>
 
 </body>
 </html>
 """
+
+nodes_edges_out = open("data.js", "w")
+nodes_edges_out.write("var graphs = " + json.dumps(graphs) + ";")
+#nodes_edges_out.write("var nodes = " + json.dumps(nodes_by_id) + ";")
+#nodes_edges_out.write("var edges = " + json.dumps(edges) + ";")
+nodes_edges_out.close()
+
 
 html_out = open("test.html", "w")
 
